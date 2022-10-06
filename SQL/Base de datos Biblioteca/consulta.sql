fd15153b87c8a8564a033e7bda539717c7b96421
Francisco Diaz Salerno <diazsalerno2@outlook.es>
2022-10-06 15:06:19 +0200

    class exercises

--- SQL/Base de datos Biblioteca/consulta.sql
+++ SQL/Base de datos Biblioteca/consulta.sql
@@ -1,10 +1,12 @@
+use francisco_biblioteca;
+
 CREATE TABLE sugerencia (
     id_sugerencia INT(5) PRIMARY KEY AUTO_INCREMENT,
     id_usuario INT(5) NOT NULL,
     titulo_sugerencia VARCHAR (100) NOT NULL,
     rating_sugerencia ENUM('1','2','3','4','5') DEFAULT '1',
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-)
+);
 
 
 INSERT INTO USUARIOS (nombre_usuario, apellido_usuario, email_usuario, num_consultas, num_prestamos, num_dias_falta) VALUES('Bill', 'Gates', 'bill.gates@gmail.com', 12, 5, 0),
