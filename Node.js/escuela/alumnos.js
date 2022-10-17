326168b03dc9bcfbe86639369e1a136b825da16e
Francisco Diaz Salerno <diazsalerno2@outlook.es>
2022-10-17 20:28:31 +0200

    Class exercises

---
+++ Node.js/escuela/alumnos.js
@@ -0,0 +1,18 @@
+const alumnos = 
+[
+{"nombre":"Zoe","apellido":"Ramirez","ciudad":"Almería","fecha_nacimiento":"2005-08-19","sexo":"M"},
+{"nombre":"David","apellido":"Schmidt","ciudad":"Almería","fecha_nacimiento":"2004-12-25","sexo":"H"},
+{"nombre":"Cristina","apellido":"Lemke","ciudad":"Almería","fecha_nacimiento":"2005-01-06","sexo":"M"},
+{"nombre":"Esther","apellido":"Spencer","ciudad":"Almería","fecha_nacimiento":"1956-12-19","sexo":"M"},
+{"nombre":"Carmen","apellido":"Streich","ciudad":"Almería","fecha_nacimiento":"1958-10-15","sexo":"M"},
+{"nombre":"Alfredo","apellido":"Stiedemann","ciudad":"Almería","fecha_nacimiento":"1957-01-01","sexo":"H"},
+{"nombre":"Manolo","apellido":"Hamill","ciudad":"Almería","fecha_nacimiento":"2005-04-23","sexo":"H"},
+{"nombre":"Alejandro","apellido":"Kohler","ciudad":"Almería","fecha_nacimiento":"1980-03-14","sexo":"H"},
+{"nombre":"Antonio","apellido":"Fahey","ciudad":"Almería","fecha_nacimiento":"1982-03-18","sexo":"H"},
+{"nombre":"Guillermo","apellido":"Ruecker","ciudad":"Almería","fecha_nacimiento":"1973-05-05","sexo":"H"},
+{"nombre":"Micaela","apellido":"Monahan","ciudad":"Almería","fecha_nacimiento":"1976-02-25","sexo":"H"},
+{"nombre":"Francesca","apellido":"Schowalter","ciudad":"Almería","fecha_nacimiento":"1980-10-31","sexo":"H"}
+]
+
+module.exports.datosAlumnos = alumnos;
+
---
+++ Node.js/escuela/mi_escuela.js
@@ -0,0 +1,55 @@
+const datos = require('./alumnos.js');
+const precioBaseMatricula = 250;
+// Para una escuela de idiomas, hace falta saber:
+// 1) que alumnos son menores de edad, ya que se pasa la información de su asistencia a los padres
+// 2) que alumnos son mayores de 65, ya que disponen de un descuento en la matrícula, a razón de
+// un 5% por cada año que pasen de 64.
+// Función que indique para los alumnos menores de edad
+// su nombre, cuantos años tienen y cuantos días les faltan para cumplir 18
+// De este modo:
+// Pepa Pi tienes 17 años y te faltan 32 días para cumplir 18 años.
+// Función que indique que alumnos pasan de 65 años, a qué descuento tienen derecho y
+// cuanto costaría su matrícula. Por ejemplo:
+// Pau Guerra tienes 66 años, tu descuento es del 10% y el importe de tu matrícula es de 225 €
+
+
+datos.datosAlumnos.forEach(
+    (alumno) => {
+        let hoy = new Date();
+        let cumpleanos = new Date(alumno.fecha_nacimiento);
+        let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        let m = hoy.getMonth() - cumpleanos.getMonth();
+
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+            edad--;
+        }
+        // Función que indique para los alumnos menores de edad
+        // su nombre, cuantos años tienen y cuantos días les faltan para cumplir 18
+        // De este modo:
+        // Pepa Pi tienes 17 años y te faltan 32 días para cumplir 18 años.
+
+        if (edad < 18) {
+            let dia_milisegundos = 86400000;      // Cantidad de milisegundos en un día
+            let diff_milisegundos = hoy - cumpleanos;
+            let diff_dias = diff_milisegundos / dia_milisegundos;
+            let dias_faltantes = Math.round(6570 - diff_dias)    // 6570 son los días que hay en 18 años
+
+            console.log(`${alumno.nombre} ${alumno.apellido} tienes ${edad} y te faltan ${dias_faltantes} días para cumplir 18 años`);
+        }
+
+        // Función que indique que alumnos pasan de 65 años, a qué descuento tienen derecho y
+        // cuanto costaría su matrícula. Por ejemplo:
+        // Pau Guerra tienes 66 años, tu descuento es del 10% y el importe de tu matrícula es de 225 €
+
+        if (edad >= 65) {
+            let añosPasados = edad - 64;
+            let descuento = (precioBaseMatricula * 0.05) * añosPasados;
+            let precioFinal = precioBaseMatricula - descuento;
+
+            console.log(alumno.nombre, alumno.apellido + ' tienes ' + edad + ' años tu descuento es del ' + ((0.05 * 100) * añosPasados) + '% y el importe de tu matrícula es de ' + precioFinal + '€');
+        }
+    }
+
+)
+
+
