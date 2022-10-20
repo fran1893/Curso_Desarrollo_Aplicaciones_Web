f580ec071c768bfaca3ee9338cab921c110273eb
Francisco Diaz Salerno <diazsalerno2@outlook.es>
2022-10-20 20:00:15 +0200

    Class exercises

--- Node.js/ejercicioTablas_v2/app.js
+++ Node.js/ejercicioTablas_v2/app.js
@@ -1,4 +1,5 @@
 const { argv } = require("process");
+const colors = require('colors');  // Importamos el paquete colores que instalamos por npm para darle color a la consola
 const fs = require('fs');
 
 // Leemos el fichero JSON y lo convertimos en un objeto para pasar los idiomas, los pasaremos como ${miJSON[argv[3]]}
@@ -6,7 +7,14 @@ const miJSON = JSON.parse(fs.readFileSync('config/idiomas.json', 'utf-8'));
 
 let eleccionIdioma = miJSON[argv[3]];
 
-// Transformamos el string para que quede en el idioma elegido y en el formato que queremos para el nombre del fichero creado
+
+// Si el idioma que elije el usuario no esta disponible o no lo reconoce termina el progarama y envia mensaje de error.
+if(eleccionIdioma == undefined){
+    console.log('Language unknown | Idioma desconocido');
+    process.exit();
+}
+
+// Transformamos el string para que quede en el idioma elegido y en el formato que queremos para el nombre del fichero creado.
 let nombreFichero_idioma = eleccionIdioma.split(' ').join('_');
 
 let tabla = `================================
@@ -23,13 +31,13 @@ for (i = 1; i <= 10; i++) {
 tabla += `================================`;
 
 
-console.log(tabla)    // Vemos la tabla en consola
+console.log(tabla.rainbow.bold)    // Vemos la tabla en consola con los colores elegidos gracias al paquete 'colors'
 
 
- // Crea un fichero con la tabla de multiplicar de dicho numero y lo guarda en su respectiva carpeta
+//  Crea un fichero con la tabla de multiplicar de dicho numero y lo guarda en su respectiva carpeta
 fs.writeFile(`${argv[3]}/${nombreFichero_idioma}_${argv[2]}.txt`, tabla, (error) => {
     if (error) {
         throw error;                                                        
     }
-    console.log('Fichero creado');
+    console.log('Fichero creado'.green.bold);
 });
