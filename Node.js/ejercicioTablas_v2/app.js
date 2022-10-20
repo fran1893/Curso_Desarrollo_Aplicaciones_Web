1f322ef4232977aeea9a65583c9237c532fa58b1
Francisco Diaz Salerno <diazsalerno2@outlook.es>
2022-10-20 19:13:32 +0200

    Class exercises

--- Node.js/ejercicioTablas_v2/app.js
+++ Node.js/ejercicioTablas_v2/app.js
@@ -4,10 +4,16 @@ const fs = require('fs');
 // Leemos el fichero JSON y lo convertimos en un objeto para pasar los idiomas, los pasaremos como ${miJSON[argv[3]]}
 const miJSON = JSON.parse(fs.readFileSync('config/idiomas.json', 'utf-8'));
 
+let eleccionIdioma = miJSON[argv[3]];
+
+// Transformamos el string para que quede en el idioma elegido y en el formato que queremos para el nombre del fichero creado
+let nombreFichero_idioma = eleccionIdioma.split(' ').join('_');
 
 let tabla = `================================
-== ${miJSON[argv[3]]} ${argv[2]} ==
+== ${eleccionIdioma} ${argv[2]} ==
   ===============================\n`;
+
+
 
 for (i = 1; i <= 10; i++) {
     tabla += `${argv[2]} x ${i} = ${argv[2] * i}\n`
@@ -17,12 +23,13 @@ for (i = 1; i <= 10; i++) {
 tabla += `================================`;
 
 
-
 console.log(tabla)    // Vemos la tabla en consola
 
-fs.writeFile(`tabla_multiplicar_${argv[2]}.txt`, tabla, (error) => {
+
+ // Crea un fichero con la tabla de multiplicar de dicho numero y lo guarda en su respectiva carpeta
+fs.writeFile(`${argv[3]}/${nombreFichero_idioma}_${argv[2]}.txt`, tabla, (error) => {
     if (error) {
-        throw error;                                                        // Crea un fichero con la tabla de multiplicar de dicho numero
+        throw error;                                                        
     }
     console.log('Fichero creado');
 });
