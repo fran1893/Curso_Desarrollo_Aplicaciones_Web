68eaaa2151cacb47e717d6fd1b3231c2b091bf58
Francisco Diaz <diazsalerno2@outlook.es>
2022-11-10 16:08:46 +0100

    Update index.js

--- Node-Docker/src/index.js
+++ Node-Docker/src/index.js
@@ -1,1 +1,14 @@
 // Aquí arrancaremos el servidor
+
+// const express = require('express');
+// app.use(express.static('public'));
+
+const app = require('./app.js');
+
+const router = require('./routes/route.js');
+
+app.use(router)
+
+
+
+app.listen(4000, ()=> console.log('Server Running....'));
