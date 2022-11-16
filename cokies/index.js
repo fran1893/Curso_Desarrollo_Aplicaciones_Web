3851be69177f9108fa64e6f4cc543157653686e1
Francisco Diaz <diazsalerno2@outlook.es>
2022-11-16 20:40:22 +0100

    Update index.js

--- cokies/index.js
+++ cokies/index.js
@@ -5,9 +5,17 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(cookieParser())
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-
+app.get('/setcookie', (req, res) =>{
+    res.cookie(
+        "theme",
+        "dark"
+    )
+    res.send('SetCookies')
+})
 
 
 app.listen(3000, ()=> console.log('Server Running...'))
