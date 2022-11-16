2f826f886a56262921bc6ca087a15bb9944b6243
Francisco Diaz <diazsalerno2@outlook.es>
2022-11-16 16:20:08 +0100

    Update index.js

--- Node.js/formulario-a-node/index.js
+++ Node.js/formulario-a-node/index.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/formulario',(req, res)=>{
-
+app.post('/formulario', async (req, res) => {
+    res.json(req.body)
 })
 
 
 
 
-app.listen(PORT, ()=>{console.log(`Escuchando por el puerto ${PORT}`)});
+app.listen(PORT, () => { console.log(`Escuchando por el puerto ${PORT}`) });
