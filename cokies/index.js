370bbe5db11e16707ed0f9b1158cf7461b26f155
Francisco Diaz <diazsalerno2@outlook.es>
2022-11-16 20:35:24 +0100

    Update index.js

--- cokies/index.js
+++ cokies/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const cookieParser = require('cookie-parser');
 
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+
 
 app.listen(3000, ()=> console.log('Server Running...'))
